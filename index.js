614fb3009e11a183df51026b5f5c64a31537dcc1
Christian Wacker <christianwwacker@gmail.com>
2020-01-07 01:32:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,3 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-
-function sayHiToGrandma(string) {
-  if `string` === lowercase
-    return "I can't hear you!"
-}
