3d1ec5060c6935eed364b645a25faa837189f7be
Christian Wacker <christianwwacker@gmail.com>
2020-01-07 01:37:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log('hello'.toUpperCase)
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase)
+  console.log('HELLO'.toLowerCase)
 }
