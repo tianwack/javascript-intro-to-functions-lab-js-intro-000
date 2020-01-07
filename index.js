c6ade525441712d7be34485cced30cfb900f8394
Christian Wacker <christianwwacker@gmail.com>
2020-01-07 02:00:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  uppercase = string.toUpperCase();
-  string = uppercase;
-  console.log(uppercase);
+  var uppercase = "HELLO"
+  uppercase.toUpperCase() === uppercase
+  console.log(string.toUpperCase);
 }
