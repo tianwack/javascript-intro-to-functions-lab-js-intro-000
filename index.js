ab24ac9a6a774f8445b813e7996baa747c4b2277
Christian Wacker <christianwwacker@gmail.com>
2020-01-07 02:14:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ function logWhisper(string) {
 logWhisper ("HELLO")
 
 function sayHiToGrandma(string) {
-  if `string` === lowercase
-    return "I can't hear you"
- else if `string` === uppercase
-    return "YES INDEED!"
- else if `string` === "I love you, Grandma."
-    return "I love you, too."
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+  else if (string === "I love you, Grandma") return "I love you, too."
 }
