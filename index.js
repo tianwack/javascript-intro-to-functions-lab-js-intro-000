fabd8e590ef8d910c06d410e3d99bee4c7b6bbc0
Christian Wacker <christianwwacker@gmail.com>
2020-01-07 02:12:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,12 @@ function logWhisper(string) {
 }
 
 logWhisper ("HELLO")
+
+function sayHiToGrandma(string) {
+  if `string` === lowercase
+    return "I can't hear you"
+} else if `string` === uppercase
+    return "YES INDEED!"
+} else if `string` === "I love you, Grandma."
+    return "I love you, too."
+}
